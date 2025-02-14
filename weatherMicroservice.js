85b2c1d80780f10a59d017e4422d0991e1bc3461
Eric Peters <ericrpeters@protonmail.com>
2025-02-14 12:49:48 -0800

    final comments

--- weatherMicroservice.js
+++ weatherMicroservice.js
@@ -7,17 +7,18 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const API_KEY = "";
-const PORT = 3000;
+const API_KEY = ""; // User needs to add API key here
+const PORT = 3000; // Optionally set up port number
 
 app.get("/weather", async (req, res) => {
     const zip = req.query.zip;
 
     try {
+        // Fetches the weather data
         const response = await fetch(
             `https://api.weatherbit.io/v2.0/forecast/daily?postal_code=${zip}&key=${API_KEY}`
         );
-
+        // Gets the data as a JSON and then responds with it
         const weatherData = await response.json();
         res.json(weatherData);
 
